edf7f24bde5d4a7c9fe7965c71af4fe00758a6b7
Aditya-46-Raj <adityaraj21103@gmail.com>
2026-06-13 16:35:23 +0530

    Product updated

---
+++ backend/src/models/Product.ts
@@ -0,0 +1,28 @@
+
+
+
+
+import mongoose, { Schema, InferSchemaType } from "mongoose";
+
+const ProductSchema = new Schema(
+ {
+ title: { type: String, required: true },
+ category: { type: String, required: true },
+ brand: { type: String },
+ originalPrice: { type: Number, required: true },
+ images: { type: [String], default: [] },
+ description: { type: String },
+ variants: {
+ sizes: { type: [String], default: [] },
+ colors: { type: [String], default: [] },
+ },
+ weightGrams: { type: Number, default: 500 },
+ },
+ { timestamps: true }
+);
+
+export type Product = InferSchemaType<typeof ProductSchema> & { _id: mongoose.Types.ObjectId };
+export const ProductModel = mongoose.model("Product", ProductSchema);
+
+
+
